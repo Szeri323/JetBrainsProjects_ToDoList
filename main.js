8fc060b19ecde28e631014ddc2c1c4d9d455825b
szeri <szeri323@gmail.com>
2022-03-02 13:24:57 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+/*document.getElementById("add-task-button").addEventListener("onclick",function addTask() {
+    console.log(document.getElementById("input-task"));
+});*/
+/*document.getElementById("add-task-button").addEventListener("onclick", function() {
+    let text = document.getElementById("input-task").value;
+    document.getElementById("input-task").value = text.toUpperCase();
+    /*console.log(text);
+});*/
